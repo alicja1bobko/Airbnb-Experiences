5f10337562f1174132fa812b97c128e2ab15526d
AlicjaBobko <alicja_bobko@o2.pl>
2022-05-13 16:20:04 +0200

    sold out and online badges

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,18 +3,23 @@ import star from '../images/star.png';
 
 
 export default function Card(props) {
+    let badgeText;
+    if (props.openSpots === 0) {
+        badgeText = 'SOLD OUT'
+    } else if (props.location === "Online") {
+        badgeText = 'ONLINE'
+    }
 
     return (
         <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img src={require(`../../public/images/${props.img}`)} alt="katie zaferes" className="card--image" />
             <div className="card--stats">
-                
                 <img src={star} alt="star" className="card--star" /> 
                 <span>{ props.rating}</span>
-                    <span className="gray">({props.reviewCount})	&#8226;</span>
+                 <span className="gray">({props.reviewCount})	&#8226;</span>
                 <span className="gray">{props.location}</span>
-                     </div>
-
+             </div>
                 <p className="card--title">{props.title} </p>
                 <p className="card--price"><span className="bold">From ${ props.price} </span>/ person</p>
         </div>
