ad8a54a5a53c5266d9ea93e1838ef362e9630b5f
AlicjaBobko <alicja_bobko@o2.pl>
2022-05-13 17:05:01 +0200

    cleaned Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,25 +3,26 @@ import star from '../images/star.png';
 
 
 export default function Card(props) {
+    const {openSpots, location, coverImg, stats, title, price } = props;
     let badgeText;
-    if (props.openSpots === 0) {
+    if (openSpots === 0) {
         badgeText = 'SOLD OUT'
-    } else if (props.location === "Online") {
+    } else if (location === "Online") {
         badgeText = 'ONLINE'
     }
 
     return (
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={require(`../../public/images/${props.img}`)} alt="katie zaferes" className="card--image" />
+            <img src={require(`../../public/images/${coverImg}`)} alt="katie zaferes" className="card--image" />
             <div className="card--stats">
                 <img src={star} alt="star" className="card--star" /> 
-                <span>{ props.rating}</span>
-                 <span className="gray">({props.reviewCount})	&#8226;</span>
-                <span className="gray">{props.location}</span>
+                <span>{stats.rating}</span>
+                 <span className="gray">({stats.reviewCount})	&#8226;</span>
+                <span className="gray">{location}</span>
              </div>
-                <p className="card--title">{props.title} </p>
-                <p className="card--price"><span className="bold">From ${ props.price} </span>/ person</p>
+                <p className="card--title">{title} </p>
+                <p className="card--price"><span className="bold">From ${price} </span>/ person</p>
         </div>
     )
 }
